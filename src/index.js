b9747e6c702072fdc59dfdc0baba245be464548b
Vyacheslav Gulyaev <vv_gulyaev@mail.ru>
2016-11-13 22:56:26 +0300

    add task3A

--- src/index.js
+++ src/index.js
@@ -1,14 +1,90 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose'
+import Promise from 'bluebird'
+//import _ from 'lodash';
+import bodyParser from 'body-parser'
+import saveDataInDb from './saveDataInDb'
+import Pet from './models/Pet';
+import User from './models/User';
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://publicdb.mgbeta.ru/vvgulyaev_skb3')
 
 const app = express();
+app.use(bodyParser.json());
 app.use(cors());
-app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
+
+app.get('/users', async (req, res) => {
+	const users = await User.find();
+	return res.json(users);
 });
+
+app.get('/pets', async (req, res) => {
+	const pets = await Pet.find().populate('owner');
+	return res.json(pets);
+});
+
+app.post('/data', async (req, res) => {
+	const data = req.body;
+	console.log(data);
+	if (!data.user) return res.status(400).send('user required');
+	if (!data.pets) data.pets = [];
+	try {
+		const result = await saveDataInDb(data);
+		return res.json(result);
+	} catch (err){
+		return res.status(500).json(err);
+	}
+});
+
+var request = require("request")
+var _ = require('lodash-getpath'); // import lodash with the mixin provided
+
+var pcUrl = "https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json";
+var baseURL = '/task3A/';
+let pc = {};
+
+request({
+    url: pcUrl,
+    json: true
+}, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+        //console.log(body) // Print the json response
+        pc = body;
+    }
+})
+
+app.get(baseURL + '*', async (req, res) => {
+	var struct_path = req.path.replace(baseURL, "");
+	struct_path = struct_path.replace(/\//g, ".");
+	var fields = struct_path.split(".");
+	var output_value = _.getPath(pc, struct_path);
+	//console.log(pc);
+	if ((!fields[0])&&(fields.length==1))
+	{
+		return res.json(pc);
+	}
+	else if ((fields[0]=='volumes') && (fields.length==1))
+	{
+		console.log(fields);
+		return res.json(pc);
+	}
+	else if (output_value===undefined)
+	{
+		return res.status(404).json("Not Found");
+	}
+	else
+	{
+		return res.json(output_value);
+
+	}
+});
+
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
+
+
